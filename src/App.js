0c52a224b983a3672d205e9a5cfcfa4f5950e309
fernando sotelo <al169888@alumnos.uacj.mx>
2022-09-04 14:09:06 -0600

    Actualizacion desde pc escritorio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MaterialUI from "./Components/MaterialUI";
+// import Bootstrap from "./Components/Bootstrap";
+// import Bulma from "./Components/Bulma";
+import ReactBootstrap from "./Components/ReactBootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Frameworks CSS con React</h1>
+      {/* <Bootstrap /> */}
+      {/* <Bulma /> */}
+      {/* <ReactBootstrap /> */}
+      <MaterialUI />
     </div>
   );
 }
